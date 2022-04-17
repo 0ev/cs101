2e0e51116e7e6f55ad91d3f035399381f4929e13
0ev <andyye.jongcye@gmail.com>
2022-04-18 03:01:15 +0900

    Add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cs101',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A library for KAIST cs101 students',
+    author='0ev',
+    license='MIT',
+)
